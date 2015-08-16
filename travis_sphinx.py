e7ba678e09e76de3e202cdda7cf52b0538818dce
Grant Mercer <Gmercer015@gmail.com>
2015-08-16 12:35:14 -0700

    Add usage and command line opts for script

--- travis_sphinx.py
+++ travis_sphinx.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import subprocess
+import getopt
 
 def run(*args):
     ret = subprocess.call(args, stdout=sys.stdout, stderr=sys.stderr)
@@ -24,5 +25,31 @@ def deploy():
         run('git', 'push', '-fq', 'https://%s@github.com/%s.git'
             % (token, repo), 'gh-pages)')
 
+def usage():
+    print 'Usage: travis-sphinx [options] {actions}\n'
+    print 'Options:\n  -h, --help\t\tProvide information on script or following action\n' + \
+          '  -s, --source\t\tSource directory of sphinx docs, default is docs/source'
+    print 'Actions:\n  build\t\tBuild sphinx documentation\n  deploy\t\tDeploy sphinx docs to travis branch'
+
 def main():
-    print 'hi'
+    source_dir = 'docs/source'
+    # Print usage if no arguments entered
+    if len(sys.argv) == 1:
+        print 'travis-sphinx v0.0.1'
+        usage()
+        exit(0)
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], 'hs:', ['help', 'source='])
+
+    except getopt.GetoptError as err:
+        print str(err) + ', see --help for valid arguments'
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt in ('-h', '--help'):
+            usage()
+            sys.exit(2)
+        elif opt in ('s', '--source'):
+            source_dir = arg
+
+    print source_dir
